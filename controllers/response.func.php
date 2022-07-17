0100ec89e1c0f826cd11acca2a1923f2713110c1
Laboratoire <laboratoire@carly-sa.fr>
2022-07-17 17:48:50 +0200

    update

--- controllers/response.func.php
+++ controllers/response.func.php
@@ -2,6 +2,11 @@
 
 class Tools
 {
+    public const TROP_LONG = "Trop d'arguments";
+    public const ERR_ARG = "Arguments érronés";
+    public const MANQUANT_ARG = "Arguments manquants";
+    public const NBR_ARG = "l'id doit être un nombre";
+
     public static function return_json(string $etat,array $data)
     {
         return json_encode(
@@ -10,6 +15,11 @@ class Tools
                 "data" => $data
             ]
         );
+    }
+
+    public static function msg_argument(string $msg)
+    {
+        throw new Exception($msg);
     }
 
     public static function sendMail($destinataire, $sujet, $message):void
--- index.php
+++ index.php
@@ -3,42 +3,53 @@
 define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http")."://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
 
 $method = $_SERVER['REQUEST_METHOD'];
-// header('Content-Type: application/json; charset=UTF-8');
+header('Content-Type: application/json; charset=UTF-8');
 
 require "./models/user/user.manager.php";
 require "./controllers/response.func.php";
+
+
+$partner_url = "partner";
+$structure_url = "structure";
+
 
 $userManage = new UserManage;
 
 try{
     $url = explode("/", filter_var($_REQUEST['page'], FILTER_SANITIZE_URL));
     if(!empty($url[0])){
-        if($url[0] == "V1" && isset($url[1])){
+        if($url[0] === "V1" && !empty($url[1])){
             switch($method){
                 case "GET":
-                    // user----------------------------------------------------------------------------------------------
-                    if($url[1] == "user-all")
-                    {
-                        echo Tools::return_json("méthode GET", $userManage->get_all_user());
-                    }
                     // partner ------------------------------------------------------------------------------------------------
-                    if($url[1] == "partner-all")
+                    if($url[1] === $partner_url."-all")
                     {
                         echo Tools::return_json("méthode GET", ['test' => "Liste partner + Droits"]);
                     }
-                    if($url[1] == "partner" && isset($url[2]) && !isset($url[3]) && is_numeric($url[2])){
-                        echo Tools::return_json("méthode GET", ['test' => "Partner avec pour id : ".$url[2]]);
+                    if($url[1] === $partner_url && isset($url[2]) && !isset($url[3]) && is_numeric($url[2]))
+                    {
+                        echo Tools::return_json("méthode GET", ['test' => "Partner avec droit pour id : ".$url[2]]);
                     }else{
-                        if(isset($url[3])){
-                            throw new Exception("Il y a trop d'argument");
-                        }else{
-                            throw new Exception("Il manque de(s) argument(s) ou argument érroné");
+                        if($url[1] === $partner_url)
+                        {
+                            isset($url[3]) ? Tools::msg_argument(Tools::TROP_LONG) : "";
+                            !is_numeric($url[2]) ? Tools::msg_argument(Tools::NBR_ARG) : "";
                         }
                     }
                     // structure --------------------------------------------------------------------------------------------------
-                    if($url[1] == "structure-all")
+                    if($url[1] === $structure_url."-all")
                     {
                         echo Tools::return_json("méthode GET", ['test' => "Liste partner + Droits"]);
+                    }
+                    if($url[1] === $structure_url && isset($url[2]) && !isset($url[3]) && is_numeric($url[2]))
+                    {
+                        echo Tools::return_json("méthode GET", ['test' => "Structure avec droit pour id : ".$url[2]]);
+                    }else{
+                        if($url[1] === $structure_url)
+                        {
+                            isset($url[3]) ? Tools::msg_argument(Tools::TROP_LONG) : "";
+                            empty($url[2]) ? Tools::msg_argument(Tools::MANQUANT_ARG) : !is_numeric($url[2]) ? Tools::msg_argument(Tools::NBR_ARG) : "";
+                        }
                     }
                 break;
                 case "POST":
@@ -57,3 +68,5 @@ try{
 catch(Exception $e){
     echo Tools::return_json("Erreur", ["message" => $e->getMessage()]);
 }
+
+
